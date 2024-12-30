b54a591d3983baba7e6dd18a4f97da73afbc66f3
cong192 <duycongib192@gmail.com>
2024-12-30 15:11:00 +0700

    checkmaster

--- src/main/java/com/poly/app/domain/model/ProductDetail.java
+++ src/main/java/com/poly/app/domain/model/ProductDetail.java
@@ -1,10 +1,8 @@
 package com.poly.app.domain.model;
 
 import com.poly.app.domain.model.base.PrimaryEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.poly.app.infrastructure.constant.EntityProperties;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -60,12 +58,10 @@ public class ProductDetail extends PrimaryEntity implements Serializable {
 
     Double weight;
 
+    @Column(columnDefinition = EntityProperties.DEFINITION_DESCRIPTION)
     String descrition;
 
     Integer status;
-
-
-
 
 
 }
